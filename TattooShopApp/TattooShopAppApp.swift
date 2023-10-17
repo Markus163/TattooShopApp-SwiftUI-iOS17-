b9084ce644a8a212085622165ebb3390787ad99c
Markus163 <pprimak@mail.ru>
2023-10-17 21:15:45 +0400

    Initial Commit

---
+++ TattooShopApp/TattooShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TattooShopAppApp.swift
+//  TattooShopApp
+//
+//  Created by Марк Михайлов on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TattooShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
